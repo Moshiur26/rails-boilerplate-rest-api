6fba59151c3ead69d13ca68d002418bf681b47f5
moshiur26 <moshiurrahmanb11@gmail.com>
2022-03-09 01:03:06 +0600

    make modification into gitignote and admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,9 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :phone, :email, :password, :password_confirmation, presence: true
+  validates :email, :phone, uniqueness: true
+
+  enum role: { admin: 0, super_admin: 1 }
 end
