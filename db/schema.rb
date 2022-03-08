4a5fb24b3b1d2e69bc6e5d7278949ed5984b3cd7
moshiur26 <moshiurrahmanb11@gmail.com>
2022-03-09 01:09:53 +0600

    create new db table for store autorizationKey table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_08_183611) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_08_190743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_183611) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "authorization_keys", force: :cascade do |t|
+    t.text "token", null: false
+    t.datetime "expiry"
+    t.bigint "authable_id", null: false
+    t.string "authable_type", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
